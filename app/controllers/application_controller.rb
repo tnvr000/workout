9762421ea87c9efa71f96de9edaa75c3e39707de
Tanveer Ahmad Khan <tanveerk@mindfiresolutions.com>
2020-07-19 19:07:31 +0530

    setup simple_form, devise and apartment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_paramaters, if: :devise_controller?
+
+    protected
+    def configure_permitted_paramaters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :subdomain])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :subdomain])
+    end
 end
